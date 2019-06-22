d173ef253fee3bd4e30f83b2c1856c030335aea9
lejaeyun <48307261+lejaeyun@users.noreply.github.com>
2019-06-23 02:07:58 +0900

    Create 10039_평균점수.py

---
+++ 6. 1차원 배열 사용하기/10039_평균점수.py
@@ -0,0 +1,14 @@
+#https://www.acmicpc.net/problem/11039
+import sys
+input = sys.stdin.readline
+
+def main() :
+   score = list()
+   for _ in range(5) :
+      s = int(input())
+      if s < 40 :
+         s = 40
+      score.append(s)
+   print(int(sum(score)/5))
+      
+main()
