4789cd07ac8b677bedd107bc83d2038950245dc7
lejaeyun <48307261+lejaeyun@users.noreply.github.com>
2019-11-26 15:01:03 +0900

    11.26

--- 대학교 수업/2학년/2학기/웹응용프로그래밍/과제/HW4/HW4.php
+++ 대학교 수업/2학년/2학기/웹응용프로그래밍/과제/HW4/HW4.php
@@ -1,21 +1,21 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>title</title>
 </head>
 <body oncontextmenu="return false" ondragstart="return false" onselectstart="return false">
-<h2>_SERVER</h2><br>
-<?php
-  foreach ( $_SERVER as $key => $value ) {
-	echo  $key . ' : '.  $value .'<br>';
-  }
-?>
-<br>
+
 <h2>_ENV</h2> <br>
 <?php
   foreach ( $_ENV as $key => $value ) {
+	echo  $key . ' : '.  $value .'<br>';
+  }
+?>
+
+<br>
+<h2>_SERVER</h2><br>
+<?php
+  foreach ( $_SERVER as $key => $value ) {
 	echo  $key . ' : '.  $value .'<br>';
   }
 ?>
